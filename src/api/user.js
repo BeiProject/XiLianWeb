e8398c874c3026e3ce492c4583df3f7b6bc2353b
BeiProject <beiproject12138@gmail.com>
2023-04-10 23:58:44 +0800

    初次提交

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,28 @@
-import request from '@/utils/request'
+import { param } from '@/utils';
+import http from '@/utils/request'
 
-export function login(data) {
-  return request({
-    url: '/vue-element-admin/user/login',
-    method: 'post',
-    data
-  })
+/**
+ * 用户登录方法
+ * @param  data 
+ * @returns 
+ */
+export async function login(data) {
+  return await http.login("/api/user/login",data);
 }
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-element-admin/user/info',
-    method: 'get',
-    params: { token }
-  })
+/**
+ * 获取登录用户信息和权限信息
+ * @param  token 
+ * @returns 
+ */
+export async function getInfo() {
+  return await http.get("/api/sysUser/getInfo",null);
 }
 
-export function logout() {
-  return request({
-    url: '/vue-element-admin/user/logout',
-    method: 'post'
-  })
+/**
+ * 退出登录
+ * @returns 
+ */
+export async function logout() {
+  return await http.post("/api/sysUser/loginOut",param)
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,14 +2,33 @@ import Cookies from 'js-cookie'
 
 const TokenKey = 'Admin-Token'
 
+/**
+ * 获取token
+ * @returns 
+ */
 export function getToken() {
   return Cookies.get(TokenKey)
 }
-
+/**
+ * 设置token
+ * @returns 
+ */
 export function setToken(token) {
   return Cookies.set(TokenKey, token)
 }
-
+/**
+ * 删除token
+ * @returns 
+ */
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
+
+/**
+ * 清空sessionStorage
+ * @returns 
+ */
+export function clearStorage() {
+  return sessionStorage.clear();
+}
+
